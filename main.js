7364d419ff0904fa431dd09014e50ec43c028a71
Justin <justin.l.chang@hotmail.com>
2022-02-17 18:18:24 -0500

    Made it so that the user can place a mark

--- main.js
+++ main.js
@@ -8,18 +8,16 @@ const gameBoard = (function(){
         for (let i = 0; i < 9; i++) {
             const square = document.querySelector(`.square${i}`);
             square.textContent = _board[i];
+            square.id = i;
         }
     }
 
     function editBoardArray(playerSymbol, boardIndex) {
         _board[boardIndex] = playerSymbol;
     }
+   
 
-    function getBoardArray() {
-        return _board;
-    }
-    
-    return {displayBoardContentToScreen, editBoardArray, getBoardArray};
+    return {board: _board, displayBoardContentToScreen, editBoardArray};
 
 })();
 
@@ -31,22 +29,66 @@ const gameState = (function(){
     let _turn = "user";
 
     function gameOver() {
-        return !gameBoard.getBoardArray().includes(""); 
+        return !gameBoard.board.includes("");
     }
 
     function changeTurn() {
         _turn = _turn == "user" ? "computer" : "user";
+        console.log(`Changed turn to ${_turn}`);
     }
 
     function getTurn() {
         return _turn;
     }
 
-    return {gameOver, changeTurn, getTurn};
+    return {changeTurn, getTurn, gameOver};
 
 
 })();
 
+
+/* Initialize the game controller object as module*/
+
+const gameController = (function(){
+
+    function enableUserClickSquares() {
+        for (let i = 0; i < 9; i++) {
+            const square = document.querySelector(`.square${i}`);
+            if (gameBoard.board[i] == "") {
+                square.addEventListener("click", placeUserMark);
+                console.log(`Event listener added to square ${square.id}`);
+            }
+        }
+    }
+
+    function disableUserClickSquares() {
+        for (let i = 0; i < 9; i++) {
+            const square = document.querySelector(`.square${i}`);
+            square.removeEventListener("click", placeUserMark);
+            console.log(`Disabled click on square ${square.id}`);
+        }
+    }
+
+    function placeUserMark(e) {
+        const squareId = e.target.id;
+        gameBoard.editBoardArray("X", squareId);
+        disableUserClickSquares();
+
+        console.log(`Placed mark at square ${squareId}`);
+        console.log(`Here is the new board: ${gameBoard.board}`);
+
+        // Check if game is over after every user and computer move
+        gameState.changeTurn();
+        gameBoard.displayBoardContentToScreen();
+
+    }
+
+    return {enableUserClickSquares};
+
+})();
+
+
 gameBoard.editBoardArray("X", 1);
 gameBoard.displayBoardContentToScreen();
-console.log(gameState.gameOver());
+gameController.enableUserClickSquares();
+
