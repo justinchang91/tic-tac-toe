c75d98327199de94946621ae41e49f0a8d39c626
Justin <justin.l.chang@hotmail.com>
2022-02-17 14:52:30 -0500

    Created and edited the gameBoard and gameState modules. Need to make the gameController module and add a function to allow user to edit the board

--- main.js
+++ main.js
@@ -1,15 +1,52 @@
-/* Initialize the game board object*/
+/* Initialize the game board object as a module */
 
 const gameBoard = (function(){
 
-    // Initialize the gameboard array as private variable
-    let _board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"];
+    let _board = ["", "", "", "", "", "", "", "", ""];
 
-    // Displays the contents inside board array onto HTML
     function displayBoardContentToScreen() {
+        for (let i = 0; i < 9; i++) {
+            const square = document.querySelector(`.square${i}`);
+            square.textContent = _board[i];
+        }
+    }
 
+    function editBoardArray(playerSymbol, boardIndex) {
+        _board[boardIndex] = playerSymbol;
+    }
+
+    function getBoardArray() {
+        return _board;
     }
     
-    // Return object
-    return {};
+    return {displayBoardContentToScreen, editBoardArray, getBoardArray};
+
 })();
+
+
+/* Initialize the game state object as a module */
+
+const gameState = (function(){
+
+    let _turn = "user";
+
+    function gameOver() {
+        return !gameBoard.getBoardArray().includes(""); 
+    }
+
+    function changeTurn() {
+        _turn = _turn == "user" ? "computer" : "user";
+    }
+
+    function getTurn() {
+        return _turn;
+    }
+
+    return {gameOver, changeTurn, getTurn};
+
+
+})();
+
+gameBoard.editBoardArray("X", 1);
+gameBoard.displayBoardContentToScreen();
+console.log(gameState.gameOver());
