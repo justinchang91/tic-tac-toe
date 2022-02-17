215434ee5ce6b41ff2a00440d94351780d37f076
Justin <justin.l.chang@hotmail.com>
2022-02-16 20:58:53 -0500

    Added my module declaration for gameBoard

---
+++ main.js
@@ -0,0 +1,15 @@
+/* Initialize the game board object*/
+
+const gameBoard = (function(){
+
+    // Initialize the gameboard array as private variable
+    let _board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"];
+
+    // Displays the contents inside board array onto HTML
+    function displayBoardContentToScreen() {
+
+    }
+    
+    // Return object
+    return {};
+})();
